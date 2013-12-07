a95c7094a1f5f634721bb564e1a233090b7a9249
gitjake <git@jakefoley.com>
2013-12-07 22:59:47 +0100

    Removed deprecated ereg

--- capt.php
+++ capt.php
@@ -28,7 +28,7 @@ $c=0;
 $d=0;
 echo('var captPieces = [[');
 while($row=mysql_fetch_array($f, MYSQL_ASSOC)){
-	if(ereg("white",$row['curColor']))
+	if(false !== stripos($row['curColor'], 'white'))
 		$c++;
 	if($c==1){
 		echo"], [";
